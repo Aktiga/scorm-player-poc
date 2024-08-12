e8b3e871a97e32882334995c91c7de7e45f1f6a0
Olivia Elliott <olivelliott48@gmail.com>
2024-08-12 17:00:16 -0400

    feat(assets): add xAPI packages and content

---
+++ public/xapi/creating-a-get-outdoors-101-program-xapi/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://O7pY8v2Gv7QWMrSVeZUDeg6X02P_G2IY_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Creating a &#34;Get Outdoors&#34; 101 Program"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
